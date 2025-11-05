7a38c2c1b1bbbd941770e6c2759919c788061fa0
Krishna-2311 <goyal.krishna9368@gmail.com>
2025-11-05 12:14:54 +0800

    Add funtion

---
+++ Javascript/58Function/index.js
@@ -0,0 +1,26 @@
+function nice(name) {
+    console.log("Hey " + name + " you are nice!")
+    console.log("Hey " + name + " you are very nice!")
+    console.log("Hey " + name + " you are good!")
+    console.log("Hey " + name + " your tshirt is good!")
+}
+
+
+function sum(a,b,c=3){
+    return(a+b+c)
+}
+result1=sum(87,5)
+result2=sum(10,4,6)
+console.log("The sum of these numbers is:",result1)
+console.log("The sum of these numbers is:",result2)
+
+const func1=(x)=>{
+    console .log ("I am arrow function",x)
+}
+func1(43)
+func1(56)
+
+
+
+
+
